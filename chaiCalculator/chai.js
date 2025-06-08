0e263ceb11e1fc6a1b3a849bb5bb36a3d5d1904a
Mike Muteithia <mikemuteithia@gmail.com>
2025-06-08 17:42:53 +0300

    Wrote the JS function for chai.js

---
+++ chaiCalculator/chai.js
@@ -0,0 +1,71 @@
+/* function calculatorChaiIngredients(numberOfCups) {
+    let ingredients = {
+    water: 200,
+    milk: 50,
+    teaLeaves: 1,
+    sugar: 2
+}  
+    const ingredientsAmount = {
+        waterAmount: "Water: " + ingredients["water"] * numberOfCups + "ml",
+        milkAmount: "Milk: " + ingredients["milk"] * numberOfCups + "ml",
+        teaLeavesAmount: "Tea Leaves (Majani): " + ingredients["teaLeaves"] * numberOfCups + " tbsp",
+        sugarAmount: "Sugar (Sukari): " + ingredients["sugar"] * numberOfCups + " tsp",
+      }
+
+    // return ingredientsAmount;
+
+    let msg = {
+        message: "To brew " + numberOfCups + " cup(s) of superb Kenyan Chai, asseemble the following:",
+        theFollowing: ingredientsAmount["waterAmount", "milkAmount", "teaLeavesAmount", "sugarAmount"],
+        endMsg: "Chai Tamu!" 
+    };
+    let cupsOfTea = prompt("Hello, there. Chai Master! How many cups of Chai would you like to chef today?") 
+    if (numberOfCups = `${number}`)
+        return msg
+}
+
+console.log(calculatorChaiIngredients(1)); */
+
+function calculatorChaiIngredients(numberOfCups) {
+    let ingredients = {
+    water: 200,
+    milk: 50,
+    teaLeaves: 1,
+    sugar: 2
+}  
+    const ingredientsAmount = {
+        waterAmount: `Water: ${ingredients["water"] * numberOfCups}ml`,
+        milkAmount: `Milk: ${ingredients["milk"] * numberOfCups}ml`,
+        teaLeavesAmount: `Tea Leaves (Majani): ${ingredients["teaLeaves"] * numberOfCups}tbsp`,
+        sugarAmount: `Sugar (Sukari): ${ingredients["sugar"] * numberOfCups}tsp`,
+      }
+
+    let msg = 
+    `To make ${numberOfCups} cup(s) of superb Kenyan Chai, assemble the following:
+
+    ${ingredientsAmount.waterAmount}
+    ${ingredientsAmount.milkAmount}
+    ${ingredientsAmount.teaLeavesAmount}
+    ${ingredientsAmount.sugarAmount}
+
+    Chai Tamu!`
+
+    return msg;
+}
+
+console.log(calculatorChaiIngredients(1));
+
+function promptMsg(){
+    let cupsofTea = prompt("Hello, there. Chai Master! How many cups of Chai would you like to chef today?");
+    let numberOfCups = parseInt(cupsofTea);
+
+    if (numberOfCups > 0) {
+        return calculatorChaiIngredients(numberOfCups);
+    } else {
+        return "Please enter a valid number of cups."
+    }
+}
+
+
+
+
